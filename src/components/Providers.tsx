ce4e44f72b52e2c71407e928df1ffa1a09053e4b
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-08-10 22:55:06 +0700

    [FEAT]:Add mantine cache

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,8 @@
 "use client";
-
-import { MantineProvider } from "@mantine/core";
+import { CacheProvider } from "@emotion/react";
+import { MantineProvider, useEmotionCache } from "@mantine/core";
 import { Sarabun } from "next/font/google";
+import { useServerInsertedHTML } from "next/navigation";
 
 const sarabun = Sarabun({
   subsets: ["thai"],
@@ -9,5 +10,31 @@ const sarabun = Sarabun({
 });
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <MantineProvider withGlobalStyles withNormalizeCSS theme={{fontFamily:sarabun.style.fontFamily}}>{children}</MantineProvider>;
+  const cache = useEmotionCache();
+  cache.compat = true;
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={
+        `${cache.key} ${Object.keys(cache.inserted).join(" ")}`
+      }
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(" "),
+      }}
+    />
+  ));
+
+
+
+  return (
+    <CacheProvider value={cache}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ fontFamily: sarabun.style.fontFamily }}
+      >
+        {children}
+      </MantineProvider>
+    </CacheProvider>
+  );
 }
